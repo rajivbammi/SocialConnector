07b83c0c97cb69c0d74a928de703be2861edf902
rajivbammi <bammi.rajiv@gmail.com>
2015-10-17 13:08:24 -0700

    Adding tabs support

--- SocialConnector/ContainerViewController.swift
+++ SocialConnector/ContainerViewController.swift
@@ -11,13 +11,27 @@ import UIKit
 class ContainerViewController: UIViewController {
 
     @IBOutlet weak var contentPanelView: UIView!
+    @IBOutlet var buttons: [UIButton]!
+    var selectedIndex: Int = -1
+    
     // Controllers
     var loginViewController: LoginViewController!
+    @IBAction func didPressTab(sender: AnyObject) {
+        print("Inside didPressTab: \(sender.tag)")
+    }
+    
     var contentViewController: UINavigationController!
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("inside container")
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        loginViewController = storyboard.instantiateViewControllerWithIdentifier("LoginViewController") as! LoginViewController
+        contentViewController = storyboard.instantiateViewControllerWithIdentifier("TweetNagivationController") as! UINavigationController
+        
+        
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "userLogin", name: userDidLoginNotification, object: nil)
          NSNotificationCenter.defaultCenter().addObserver(self, selector: "userLogout", name: userDidLogoutNotification, object: nil)
 
@@ -27,14 +41,15 @@ class ContainerViewController: UIViewController {
         print("Printing twitter user")
         if (TwitterUser.currentTwitterUser != nil) {
             print("inside if")
-            contentViewController = storyboard!.instantiateViewControllerWithIdentifier("TweetNagivationController") as! UINavigationController
+            showTabs()
+            self.selectedIndex = 0
             addViewControllers(contentViewController, destView: contentPanelView)
         } else {
             print("inside Containerview: Twitter User is null. Calling login")
-            self.loginViewController = storyboard!.instantiateViewControllerWithIdentifier("LoginViewController") as! LoginViewController
+            hideTabs()
+            self.selectedIndex = -1
             addViewControllers(self.loginViewController, destView: contentPanelView)
         }
-        // Do any additional setup after loading the view.
     }
 
     func addViewControllers (viewController: UIViewController, destView: UIView) {
@@ -46,28 +61,38 @@ class ContainerViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func userLogin() {
         print("Inside Containerview: inside user did Login function")
         if (self.contentViewController == nil) {
             print("Inside Containerview: self.contentViewController == nil")
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            self.contentViewController = storyboard.instantiateViewControllerWithIdentifier("TweetNagivationController") as! UINavigationController
             self.addChildViewController(self.contentViewController)
             print("Inside Containerview: added TweetNagivationController as child")
         }
         print("Inside Containerview:here")
         self.contentPanelView.addSubview(self.contentViewController.view)
-        //self.contentViewController.view.frame = self.view.frame
-        //self.contentViewController.didMoveToParentViewController(self)
+        self.contentViewController.view.frame = self.view.frame
+        self.contentViewController.didMoveToParentViewController(self)
+        showTabs()
     }
     
     func userLogout() {
         print("Inside ContainerviewController: userlogout", terminator: "")
-        self.loginViewController = storyboard!.instantiateViewControllerWithIdentifier("LoginViewController") as! LoginViewController
         addViewControllers(self.loginViewController, destView: contentPanelView)
     }
 
+    func hideTabs() {
+        print("hidding buttons")
+
+        for btn in buttons {
+            btn.hidden = true
+        }
+    }
+    func showTabs() {
+        print("showing buttons")
+        for btn in buttons {
+            btn.hidden = false
+        }
+    }
 }
